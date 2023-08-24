6e38b3f3cced2c6f9ad6c3cd116ac328aa7c6b05
eric-kenji <eric-kenji>
2023-08-24 12:44:56 +0000

    add widget parameters

--- api-data-silver.py
+++ api-data-silver.py
@@ -1,8 +1,10 @@
 # Databricks notebook source
 # Select specific columns and rename it
 
-df_cep_silver = spark.read.table("dev.cep_bronze")\
+var_environment = dbutils.widgets.get("var_environment")
+
+df_cep_silver = spark.read.table(f"{var_environment}.cep_bronze")\
     .select("bairro","cep","complemento","localidade","logradouro","uf")\
     .withColumnRenamed("logradouro","rua")
 
-df_cep_silver.write.saveAsTable("dev.cep_silver")
+df_cep_silver.write.saveAsTable(f"{var_environment}.cep_silver")
